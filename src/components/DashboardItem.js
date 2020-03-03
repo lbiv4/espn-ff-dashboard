07bf8f3a953b32ed99fdf9171cb75499ed9559ca
lbiv4 <lanebartoniv@gmail.com>
2020-03-02 16:19:43 -0800

    Fixed major dashboard item issue that was preventing rerenderinggit status!

--- src/components/DashboardItem.js
+++ src/components/DashboardItem.js
@@ -13,34 +13,61 @@ class DashboardItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalOpen: false,
-      info: props.itemInfo,
-      data: props.itemData
+      modalOpen: false
     };
+  }
+
+  componentDidMount() {
+    /*let container = document.getElementById(this.getIdFromTitle());
+    let children = container.childNodes;
+    children.forEach(child => {
+      if (child.hasChildNodes()) {
+        child.style.display = "initial";
+      } else {
+        child.style.display = "none";
+      }
+    });*/
+  }
+
+  getIdFromTitle() {
+    return this.props.title.toLowerCase().replace(/ /g, "-");
   }
 
   toggleModal() {
     this.setState({ modalOpen: !this.state.modalOpen });
+    console.log("Modal");
+    console.log(this.state.data);
   }
 
   render() {
     return (
-      <Container className="dashboard-item">
-        <Container className="item-info">{this.state.info}</Container>
-        <Container className="item-data">
-          <Button color="primary" onClick={this.toggleModal.bind(this)}>
-            Expand
-          </Button>
-          {this.props.dataItem}
-          <Modal
-            isOpen={this.state.modalOpen}
-            toggle={this.toggleModal.bind(this)}
+      <Container id={this.getIdFromTitle()} className="dashboard-item">
+        <h3>{this.props.title}</h3>
+        <Container fluid className="dashboard-item-content">
+          <Container
+            className="item-info"
+            style={{ width: `${100 - this.props.infoDataSplit}%` }}
           >
-            <ModalHeader toggle={this.toggleModal.bind(this)}>
-              Modal title
-            </ModalHeader>
-            <ModalBody>{this.props.dataItem}</ModalBody>
-          </Modal>
+            <Button color="primary" onClick={this.toggleModal.bind(this)}>
+              Expand
+            </Button>
+            {this.props.itemInfo}
+          </Container>
+          <Container
+            className="item-data"
+            style={{ width: `${this.props.infoDataSplit}%` }}
+          >
+            {this.props.itemData}
+            <Modal
+              isOpen={this.state.modalOpen}
+              toggle={this.toggleModal.bind(this)}
+            >
+              <ModalHeader toggle={this.toggleModal.bind(this)}>
+                {this.props.title}
+              </ModalHeader>
+              <ModalBody>{this.props.itemData}</ModalBody>
+            </Modal>
+          </Container>
         </Container>
       </Container>
     );
